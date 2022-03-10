8b5d6639b76ba0f9b10eabac1dfca683dfa4a421
Faiz E Arsal <93216129+FaizArsal@users.noreply.github.com>
2022-03-10 20:41:16 +0500

    wot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/navbar'
+import Bottombar from './components/bottombar'
+import Glass from './images/a.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+     render(){
+       return(
+         <div style={{overflowX: 'hidden'}}>
+         <Navbar />
+         <div style={{backgroundColor: 'lightgray'}}>
+					<div style={{paddingLeft: 100, paddingRight: 100, paddingBottom: 100, paddingTop: 50}}>
+						<h1>NEW PRODUCT</h1>
+						<h2>The Twist of Healthy Yogurt</h2>
+						<p>This website template has been designed by <br></br>Faiz ARSAL<br></br> for you, for free. You can replace all this text with your own text. </p>
+            <img src={Glass} style={{float: 'right', position: 'absolute', top: '100%', right: 100}}></img>
+          </div>
+				</div>
+         <Bottombar />
+         </div>
+       )
+     }
 }
 
-export default App;
+
